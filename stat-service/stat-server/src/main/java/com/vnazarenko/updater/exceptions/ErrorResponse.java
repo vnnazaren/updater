481a7ad1915c93f21010d4d18eea4610c53a26d1
Vladimir Nazarenko <vnazaren@yandex.ru>
2024-07-30 21:41:00 +0300

    Minor changes in Stat-server

--- stat-service/stat-server/src/main/java/com/vnazarenko/updater/exceptions/ErrorResponse.java
+++ stat-service/stat-server/src/main/java/com/vnazarenko/updater/exceptions/ErrorResponse.java
@@ -7,16 +7,9 @@ import lombok.Getter;
 /**
  * Класс с сообщением об ошибке
  */
-@Getter
 @Builder
-@AllArgsConstructor
-public class ErrorResponse {
-
-    private String message;
-
-    private String reason;
-
-    private String status;
-
-    private String timestamp;
+public record ErrorResponse(String message,
+                            String reason,
+                            String status,
+                            String timestamp) {
 }
