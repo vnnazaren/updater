f10f7846bc3c36ab12a3ce2b8a7b3ea1b0064e57
Vladimir Nazarenko <vnazaren@yandex.ru>
2024-08-06 23:57:09 +0300

    Minor changes

--- stat-service/stat-client/src/main/java/com/vnazarenko/updater/exceptions/ErrorHandler.java
+++ stat-service/stat-client/src/main/java/com/vnazarenko/updater/exceptions/ErrorHandler.java
@@ -1,18 +1,15 @@
 package com.vnazarenko.updater.exceptions;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorMessage> handleThrowable(final Throwable t) {
-        log.info("HttpStatus.INTERNAL_SERVER_ERROR (500). {}", t.getMessage(), t);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorMessage(t.getMessage()));
--- stat-service/stat-server/src/main/java/com/vnazarenko/updater/exceptions/ErrorResponse.java
+++ stat-service/stat-server/src/main/java/com/vnazarenko/updater/exceptions/ErrorResponse.java
@@ -1,8 +1,6 @@
 package com.vnazarenko.updater.exceptions;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
 /**
  * Класс с сообщением об ошибке
